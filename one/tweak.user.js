afb07dbb332fabbd28c12f2e0a43312a8f3c9e73
don Pablo <don@obeyi.com>
2020-11-04 11:49:32 +0200

    update

---
+++ one/tweak.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name    don[Pablo]: One
+// @description  don[Pablo]: One
+// @note    don[Pablo]: One
+// ### FILES
+// @update      https://raw.githubusercontent.com/donPabloNow/tweaks/main/one/tweak.user.js
+// @icon    https://raw.githubusercontent.com/donPabloNow/tweaks/main/one/icon.svg
+// @require https://raw.githubusercontent.com/donPabloNow/tweaks/main/one/scripts.js
+// @resource  Style   https://raw.githubusercontent.com/donPabloNow/tweaks/main/one/styles.css
+// ### GRANTS
+// @grant    GM_getValue
+// @grant    GM.getValue
+// @grant    GM_setValue
+// @grant    GM.setValue
+// @grant    GM_addStyle
+// @grant    GM_getResourceUrl
+// @grant    GM.getResourceUrl
+// @grant    GM_xmlhttpRequest
+// @grant    GM_getResourceText
+// @grant    GM_registerMenuCommand
+// @grant    unsafeWindow
+// @grant    GM_setClipboard
+// @grant    window.close
+// @grant    window.focus
+// @grant    window.onurlchange
+// ### INCLUDES
+// @include    *
+// ### DON PABLO
+// @version    1.0.0
+// @author    don[Pablo]
+// @create     2018-11-25
+// @license    MIT-3.0-only
+// @namespace  don@OBEYi.com
+// @copyright  2018-2020, MIT
+// @home-url   https://OBEYi.com
+// @supportURL  https://donPabloNow.com
+// @homepageURL  https://donPabloNow.com
+// @feedback-url  https://donPabloNow.com/contact
+// ==/UserScript==
+GM_addStyle(GM_getResourceText ("Style"));
