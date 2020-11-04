ec6699241fbc57c9deb3c35535cf81c886b3f5b6
don Pablo <don@obeyi.com>
2020-11-04 16:40:46 +0200

    update

--- tweak.user.js
+++ tweak.user.js
@@ -26,7 +26,7 @@
 // ### INCLUDES
 // @include    *
 // ### DON PABLO
-// @version    1.0.0
+// @version    1.0.1
 // @author    don[Pablo]
 // @create     2018-11-25
 // @license    MIT-3.0-only
@@ -39,15 +39,27 @@
 // ==/UserScript==
 GM_addStyle(GM_getResourceText("Style"));
 
-function like() {
-  !document.querySelector('svg[aria-label="Unlike"]')
-    ? document.querySelector(".fr66n button.wpO6b").click()
-    : setTimeout(function () {
+const INTER = 100;
+
+const Types = [
+  {
+  Pattern: document.URL.includes("gram.com/p/"),
+  Condition: `svg[aria-label="Unlike"]`,
+  Action: `.fr66n button.wpO6b`,
+  }
+]
+
+function action(condition, action) {
+  !document.querySelector(condition)
+    ? document.querySelector(action).click()
+    : setInterval(function () {
         close();
-      }, 500);
+      }, INTER);
 }
 
-if (document.URL.includes("gram.com/p/"))
+Types.forEach(type => {
+  if (URL.includes(type.Pattern))
   setInterval(function () {
-    like();
-  }, 500);
+    action(type.Condition, type.Action);
+  }, INTER);  
+});
