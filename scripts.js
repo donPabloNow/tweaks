7b19c6a4749063809af4df6400c7db0395ad117a
don Pablo <don@obeyi.com>
2020-11-05 13:11:07 +0200

    Revert "update"
    This reverts commit 72c31d5ef11b5539eb17f996c0d2cf6ae405af85.

--- scripts.js
+++ scripts.js
@@ -5,39 +5,23 @@ const Types = [
     Pattern: document.URL.includes("gram.com/p/"),
     Condition: `svg[aria-label="Unlike"]`,
     Action: `.fr66n button.wpO6b`,
-    Multi: false,
-    Action: false,
+    Multi: false
   },
   {
     Pattern: document.URL.includes("ViewPostListAgain"),
     Condition: `.ml-sm-5 .row a`,
     Action: `.ml-sm-5 .row a`,
-    Multi: true,
-    Action: true,
+    Multi: true
   },
 ];
-
-document.head.appendChild(
-  `<script>document.querySelector('one').onclick = () {one.Go(\`+ type +\`)}</script>`
-);
-
-var one = {
-  Go: function (type) {
-    !document.querySelector(type.Condition.length)
-      ? type.Multi
-        ? document.querySelectorAll(type.Action)[i].click()
-        : document.querySelector(type.Action).click()
-      : setInterval(function () {
-          close();
-        }, INTER);
-  },
-};
 
 Types.forEach((type) => {
   if (URL.includes(type.Pattern))
     setInterval(function () {
-      type.Action
-        ? one.Go(type)
-        : document.body.appendChild(`<one><button><p>go</p></button></one>`);
+      !document.querySelector(type.Condition.length)
+        ? (type.Multi) ? document.querySelectorAll(type.Action)[i].click() : document.querySelector(type.Action).click()
+        : setInterval(function () {
+            close();
+          }, INTER);
     }, INTER);
 });
