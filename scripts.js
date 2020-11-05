e1ca672a5896aaeeaae4e3d80b7ab5744ef79d34
don Pablo <don@obeyi.com>
2020-11-05 13:11:12 +0200

    Revert "update"
    This reverts commit 5e3b4ad4ba52d807dade403768b99498f58c9296.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
+
 const INTER = 100;
 
 const Types = [
   {
-    Pattern: document.URL.includes("gram.com/p/"),
-    Condition: `svg[aria-label="Unlike"]`,
-    Action: `.fr66n button.wpO6b`,
-  },
-];
+  Pattern: document.URL.includes("gram.com/p/"),
+  Condition: `svg[aria-label="Unlike"]`,
+  Action: `.fr66n button.wpO6b`,
+  }
+]
 
-Types.forEach((type) => {
+function action(condition, action) {
+  !document.querySelector(condition)
+    ? document.querySelector(action).click()
+    : setInterval(function () {
+        close();
+      }, INTER);
+}
+
+Types.forEach(type => {
   if (URL.includes(type.Pattern))
-    setInterval(function () {
-      !document.querySelector(type.Condition)
-        ? document.querySelector(type.Action).click()
-        : setInterval(function () {
-            close();
-          }, INTER);
-    }, INTER);
+  setInterval(function () {
+    action(type.Condition, type.Action);
+  }, INTER);  
 });
