f98399e055dd9259aedf885297e7849429f99182
don Pablo <don@obeyi.com>
2020-11-05 13:11:10 +0200

    Revert "update"
    This reverts commit f865ab4172af2a871d10cb289947a6649a7a2237.

--- scripts.js
+++ scripts.js
@@ -5,21 +5,14 @@ const Types = [
     Pattern: document.URL.includes("gram.com/p/"),
     Condition: `svg[aria-label="Unlike"]`,
     Action: `.fr66n button.wpO6b`,
-    Multi: false
-  },
-  {
-    Pattern: document.URL.includes("ViewPostListAgain"),
-    Condition: `.ml-sm-5 .row a`,
-    Action: `.ml-sm-5 .row a`,
-    Multi: true
   },
 ];
 
 Types.forEach((type) => {
   if (URL.includes(type.Pattern))
     setInterval(function () {
-      !document.querySelector(type.Condition.length)
-        ? (type.Multi) ? document.querySelectorAll(type.Action)[i].click() : document.querySelector(type.Action).click()
+      !document.querySelector(type.Condition)
+        ? document.querySelector(type.Action).click()
         : setInterval(function () {
             close();
           }, INTER);
