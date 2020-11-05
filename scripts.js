b97bf1f24592022a6fd6121bb4b312a5f8b26bb1
don Pablo <don@obeyi.com>
2020-11-05 13:11:16 +0200

    Revert "update"
    This reverts commit 237789f1fe9cf0c35f158eaca2a5094c111701e2.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,79 @@
+let base = {
+  Script: "/script.js",
+  Style: "/style.css",
+  loadExternalResource: function (url, perm) {
+    if (!url) return;
 
-const INTER = 100;
+      var type;
 
-const Types = [
-  {
-  Pattern: document.URL.includes("gram.com/p/"),
-  Condition: `svg[aria-label="Unlike"]`,
-  Action: `.fr66n button.wpO6b`,
-  }
-]
+    return new Promise(function (resolve, reject) {
+      var match = url.match(/\.([^.]+)$/);
+      if (match) type = match[1];
 
-function action(condition, action) {
-  !document.querySelector(condition)
-    ? document.querySelector(action).click()
-    : setInterval(function () {
-        close();
-      }, INTER);
-}
+      var tag;
 
-Types.forEach(type => {
-  if (URL.includes(type.Pattern))
-  setInterval(function () {
-    action(type.Condition, type.Action);
-  }, INTER);  
+      if (type) {
+        if (type === "css") {
+          tag = document.createElement("link");
+          tag.type = "text/css";
+          tag.rel = "stylesheet";
+          tag.href = url;
+        } else if (type === "js") {
+          tag = document.createElement("script");
+          tag.type = "text/javascript";
+          tag.src = url;
+        }
+
+        if (tag) {
+          tag.onload = function () {
+            resolve(url);
+          };
+          tag.onerror = function () {
+            reject(url);
+          };
+
+          if (!perm) {
+            tag.classList.add("oneStyles");
+            document.getElementById("mean-content").appendChild(tag);
+          } else {
+            document.head.appendChild(tag);
+          }
+        }
+      }
+    });
+  },
+};
+
+let go = {
+  URL: "https://tweaks.obeyi.com/",
+  _libs: [],
+  loadMultipleExternalResources: function (itemsToLoad, perm) {
+    var promises = itemsToLoad.map(function (url) {
+      if (url) return base.loadExternalResource(url, perm);
+    });
+    return Promise.all(promises);
+  },
+  check: function (condition) {
+    return document.URL.includes(condition);
+  },
+  OPTIONS: [
+    {
+      PATH: "auto.like",
+      TYPE: base.Script,
+      CON: `gram.com/p/`,
+    },
+    {
+      PATH: "dark.theme",
+      TYPE: base.Style,
+      CON: `*`,
+    },
+  ],
+};
+
+//prep
+go.OPTIONS.forEach((option) => { go.check(option.CON) ? go._libs.push(go.URL + option.PATH + option.TYPE) : null });
+
+//go
+go.loadMultipleExternalResources(go._libs, true).then(function () {
+  // shake-a-leg
 });
