4c616ff75977558b3accf58eab8fad2a9682933f
don Pablo <don@obeyi.com>
2020-11-06 03:26:52 +0200

    fg

--- one/scripts.js
+++ one/scripts.js
@@ -4,12 +4,15 @@ function CHECK(page) {
   return document.URL.includes(page);
 }
 
-if (CHECK("gram.com/p/")) {
-  setInterval(function () {
-    if (document.querySelector('svg[aria-label="Unlike"]')) {
-      document.querySelector(".fr66n button.wpO6b").click();
-    } else {
-      close();
-    }
-  }, WAIT);
-}
+(function () {
+  "use strict";
+  if (CHECK("gram.com/p/")) {
+    setInterval(function () {
+      if (document.querySelector('svg[aria-label="Unlike"]')) {
+        document.querySelector(".fr66n button.wpO6b").click();
+      } else {
+        close();
+      }
+    }, WAIT);
+  }
+})();
