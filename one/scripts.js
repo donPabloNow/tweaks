f76c53b70b2276db8d83f45206a1ddc3b9227781
don Pablo <don@obeyi.com>
2020-11-08 15:05:41 +0200

    Update scripts.js

--- one/scripts.js
+++ one/scripts.js
@@ -1,5 +1,6 @@
 const WAIT = 500;
 
+<<<<<<< HEAD
 function CHECK(page) {
   return document.URL.includes(page);
 }
@@ -11,3 +12,16 @@ if (CHECK("gram.com/p/")) {
       : close();
   }, WAIT);
 }
+=======
+if (check("gram.com/p/"))
+  (function () {
+    "use strict";
+    setInterval(function () {
+      !document.querySelector('svg[aria-label="Unlike"]')
+        ? document.querySelector(".fr66n button.wpO6b").click()
+        : setTimeout(function () {
+            close();
+          }, 5000);
+    }, 500);
+  })();
+>>>>>>> parent of ab359d4... update
