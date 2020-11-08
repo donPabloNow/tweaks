38e502736f9691e23ce7897dd44c3cd035c6a48e
don Pablo <don@obeyi.com>
2020-11-08 15:05:31 +0200

    Revert "update"
    This reverts commit 67dc629807cf3c2eed841f66631c8d3ef7a26e46.

--- one/scripts.js
+++ one/scripts.js
@@ -6,10 +6,8 @@ function CHECK(page) {
 
 if (CHECK("gram.com/p/")) {
   setInterval(function () {
-    if (document.querySelector('svg[aria-label="Unlike"]')) {
-      document.querySelector(".fr66n button.wpO6b").click();
-    } else {
-      close();
-    }
+    !document.querySelector('svg[aria-label="Unlike"]')
+      ? document.querySelector(".fr66n button.wpO6b").click()
+      : close();
   }, WAIT);
 }
